535fa129ff50ac0ee0dc00254b84a2eddeb1ac92
Lukasz Czechowicz <jestspoko@gmail.com>
2016-11-03 13:33:02 +0100

    changed example project and demo.gif

--- Example/Reveal/ViewController.swift
+++ Example/Reveal/ViewController.swift
@@ -10,23 +10,27 @@ import UIKit
 import Reveal
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    
+    let r = Reveal()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.layoutIfNeeded()
         
-        let r = Reveal(options: [ .delay(12),
-                                  .direction(.fromLeft)])
-        r.reveal { (index) in
-            
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        r.add([label1,label2,label3,label4], options: [.delay(0.4),
+                                                       .direction(.fromLeft),
+                                                       .speed(1.0)])
+        r.reveal {
+            self.r.hide()
         }
-        
-        r.update(text: "")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
